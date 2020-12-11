b278f30409aee45a101e52fc7f278a4527d0cbc9
MasterkinG32 <lichwow_masterking@yahoo.com>
2020-12-10 22:28:32 -0800

    Update config.

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.Locale = 'en'
 Config.reportCooldown = 60 --seconds
 Config.warnMax = 3  --how many warn player can get before getting kicked?
 Config.adminRanks = { -- change this as your server ranking ( default are : superadmin | admin | moderator )
-				'superadmin',
+				--'superadmin',
 				'admin',
 				'moderator',
 				--'jradmin',
