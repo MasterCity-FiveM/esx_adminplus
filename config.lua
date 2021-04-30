09c246410be8fd81814a4725bfb724decf5192cf
MasterkinG32 <lichwow_masterking@yahoo.com>
2021-04-30 14:06:00 +0430

    Cleanup skin.

--- config.lua
+++ config.lua
@@ -39,6 +39,22 @@ Config.TeleportLocations = {
 	['zendan'] = {x = 739.2527, y = 131.1956},       -- Zendan Markazi
 }
 
+Config.CleanSkin = {
+	['tshirt_1'] = 0,  ['tshirt_2'] = 0,
+	['torso_1'] = 0,   ['torso_2'] = 0,
+	['decals_1'] = 0,   ['decals_2'] = 0,
+	['arms'] = 0,
+	['pants_1'] = 0,   ['pants_2'] = 0,
+	['shoes_1'] = 0,   ['shoes_2'] = 0,
+	['helmet_1'] = -1,  ['helmet_2'] = 0,
+	['mask_1'] = 0,  ['mask_2'] = 0,
+	['chain_1'] = 0,    ['chain_2'] = 0,
+	['ears_1'] = 0,     ['ears_2'] = 0,
+	['bags_1'] = 0,     ['bags_2'] = 0,
+	['hair_1'] = 0,     ['hair_2'] = 0,
+	['bproof_1'] = 0,  ['bproof_2'] = 0
+}
+
 Config.Admin = {
     [10] = {
         male = {
@@ -360,20 +376,4 @@ Config.Admin = {
             ['bproof_1'] = 0,  ['bproof_2'] = 0			
         } 
     }
-},
-
-CleanSkin = {
-	['tshirt_1'] = 0,  ['tshirt_2'] = 0,
-	['torso_1'] = 0,   ['torso_2'] = 0,
-	['decals_1'] = 0,   ['decals_2'] = 0,
-	['arms'] = 0,
-	['pants_1'] = 0,   ['pants_2'] = 0,
-	['shoes_1'] = 0,   ['shoes_2'] = 0,
-	['helmet_1'] = -1,  ['helmet_2'] = 0,
-	['mask_1'] = 0,  ['mask_2'] = 0,
-	['chain_1'] = 0,    ['chain_2'] = 0,
-	['ears_1'] = 0,     ['ears_2'] = 0,
-	['bags_1'] = 0,     ['bags_2'] = 0,
-	['hair_1'] = 0,     ['hair_2'] = 0,
-	['bproof_1'] = 0,  ['bproof_2'] = 0
 }
